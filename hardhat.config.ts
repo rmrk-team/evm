2b1faa96957d9d31a7bd74366151544954ea4c22
CicadaNCR <henr.tosca@gmail.com>
2022-01-06 15:19:26 -0600

    add optimizer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -23,7 +23,15 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
   networks: {
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
