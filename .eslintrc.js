5840dfdd3c2951f402647eb70d089516ad79ebac
Yuri <yuripetusko@gmail.com>
2022-02-14 16:19:44 +0000

    rewrite tests into typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,9 +5,10 @@ module.exports = {
     mocha: true,
     node: true,
   },
-  plugins: ["@typescript-eslint"],
   extends: [
+    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     "standard",
+    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     "plugin:prettier/recommended",
     "plugin:node/recommended",
   ],
