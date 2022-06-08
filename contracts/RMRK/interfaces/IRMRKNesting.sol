4b99768e8ea582d817029e488106d644c131fa3f
steven2308 <steven2308@outlook.com>
2022-06-07 21:52:15 -0500

    Fixes tabs.

--- contracts/RMRK/interfaces/IRMRKNesting.sol
+++ contracts/RMRK/interfaces/IRMRKNesting.sol
@@ -7,47 +7,47 @@ import "./IRMRKNestingReceiver.sol";
 
 interface IRMRKNesting is IERC721 {
 
-  function ownerOf(uint256 tokenId)
-      external view returns (address owner);
+    function ownerOf(uint256 tokenId)
+    external view returns (address owner);
 
-  function rmrkOwnerOf(uint256 tokenId)
-      external view returns (
-          address,
-          uint256,
-          bool
-      );
+    function rmrkOwnerOf(uint256 tokenId)
+    external view returns (
+        address,
+        uint256,
+        bool
+    );
 
-  function _burnChildren(uint256 tokenId, address oldOwner) external;
+    function _burnChildren(uint256 tokenId, address oldOwner) external;
 
-  function addChild(
-      uint256 parentTokenId,
-      uint256 childTokenId,
-      address childTokenAddress
-  ) external;
+    function addChild(
+        uint256 parentTokenId,
+        uint256 childTokenId,
+        address childTokenAddress
+    ) external;
 
-  function acceptChild(
-      uint256 parentTokenId,
-      uint256 childTokenId
-  ) external;
+    function acceptChild(
+        uint256 parentTokenId,
+        uint256 childTokenId
+    ) external;
 
-  function rejectChild(
-      uint256 parentTokenId,
-      uint256 index
-  ) external;
+    function rejectChild(
+        uint256 parentTokenId,
+        uint256 index
+    ) external;
 
-  function removeChild(
-      uint256 parentTokenId,
-      uint256 index
-  ) external;
+    function removeChild(
+        uint256 parentTokenId,
+        uint256 index
+    ) external;
 
-  function unnestChild(
-      uint256 tokenId,
-      uint256 index
-  ) external;
+    function unnestChild(
+        uint256 tokenId,
+        uint256 index
+    ) external;
 
-  function unnestToken(
-      uint256 tokenId,
-      uint256 parentId,
-      address parentAddress
-  ) external;
+    function unnestToken(
+        uint256 tokenId,
+        uint256 parentId,
+        address parentAddress
+    ) external;
 }
