82edfb76221f18d4dcce794bacbc30fc6692c205
Stevan Bogosavljevic <stevyhacker@gmail.com>
2022-08-01 11:41:57 -0500

    Add fixture for factory test

--- test/implementations/factory.ts
+++ test/implementations/factory.ts
@@ -1,34 +1,39 @@
 import { ethers } from 'hardhat';
 import { expect } from 'chai';
-import { Contract } from 'ethers';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
 
 describe('RMRKFactory', async () => {
-  let rmrkFactory: Contract;
+  const name = 'RmrkTest';
+  const symbol = 'RMRKTST';
+  const ONE_ETH = ethers.utils.parseEther('1.0');
+  const maxSupply = 10000;
 
-  let owner: SignerWithAddress;
-  let addrs: any[];
+  async function deployFactoriesFixture() {
+    const [owner, ...signersAddr] = await ethers.getSigners();
 
-  beforeEach(async function () {
-    const [signersOwner, ...signersAddr] = await ethers.getSigners();
-    owner = signersOwner;
-    addrs = signersAddr;
+    const RMRKMultiResourceFactory = await ethers.getContractFactory('RMRKMultiResourceFactory');
+    const RMRKNestingFactory = await ethers.getContractFactory('RMRKNestingFactory');
+
+    const rmrkMultiResourceFactory = await RMRKMultiResourceFactory.deploy();
+    const rmrkNestingFactory = await RMRKNestingFactory.deploy();
+
+    return { rmrkMultiResourceFactory, rmrkNestingFactory, owner };
+  }
+
+  it('Deploy a new RMRK MultiResource contract', async function () {
+    const { rmrkMultiResourceFactory, owner } = await loadFixture(deployFactoriesFixture);
+    await expect(
+      rmrkMultiResourceFactory
+        .connect(owner)
+        .deployRMRKMultiResource(name, symbol, maxSupply, ONE_ETH),
+    ).to.emit(rmrkMultiResourceFactory, 'NewRMRKMultiResourceContract');
   });
 
-  describe('Deployment', async function () {
-    beforeEach(async function () {
-      const RMRKMultiResourceFactory = await ethers.getContractFactory('RMRKMultiResourceFactory');
-      rmrkFactory = await RMRKMultiResourceFactory.deploy();
-    });
-
-    it('Deploy a new RMRK MultiResource contract', async function () {
-      const name = 'RmrkTest';
-      const symbol = 'RMRKTST';
-      const ONE_ETH = ethers.utils.parseEther('1.0');
-      const maxSupply = 10000;
-      await expect(
-        rmrkFactory.connect(owner).deployRMRKMultiResource(name, symbol, maxSupply, ONE_ETH),
-      ).to.emit(rmrkFactory, 'NewRMRKMultiResourceContract');
-    });
+  it('Deploy a new RMRK Nesting contract', async function () {
+    const { rmrkNestingFactory, owner } = await loadFixture(deployFactoriesFixture);
+    await expect(
+      rmrkNestingFactory.connect(owner).deployRMRKNesting(name, symbol, maxSupply, ONE_ETH),
+    ).to.emit(rmrkNestingFactory, 'NewRMRKNestingContract');
   });
 });
