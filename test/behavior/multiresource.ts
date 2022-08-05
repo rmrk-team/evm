1ecc03337ec517cfda465cc37ffbab18620b4c2f
steven2308 <steven2308@outlook.com>
2022-08-05 09:31:06 +0300

    Finishes refactor for multiresource behavior from multiresource tests only.

--- test/behavior/multiresource.ts
+++ test/behavior/multiresource.ts
@@ -3,7 +3,7 @@ import { expect } from 'chai';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { BigNumber, Contract } from 'ethers';
 
-async function shouldSupportInterfaces() {
+async function shouldSupportInterfacesForResources() {
   describe('Interface support', async function () {
     it('can support IERC165', async function () {
       expect(await this.token.supportsInterface('0x01ffc9a7')).to.equal(true);
@@ -214,13 +214,6 @@ async function shouldHandleRejectsForResources(
       expect(await this.token.getFullPendingResources(tokenId)).to.eql([[resId2, resData2]]);
     });
 
-    // FIXME: Caller must set up the overwritting resource
-    it.skip('can reject resource and overwrites are cleared', async function () {
-      await this.token.rejectResource(tokenId, 0);
-
-      expect(await this.token.getResourceOverwrites(tokenId, resId2)).to.eql(BigNumber.from(0));
-    });
-
     it('can reject resource if approved', async function () {
       await this.token.connect(tokenOwner).approveForResources(approved.address, tokenId);
       await this.token.connect(approved).rejectResource(tokenId, 0);
@@ -246,13 +239,6 @@ async function shouldHandleRejectsForResources(
       expect(await this.token.getFullPendingResources(tokenId)).to.eql([]);
     });
 
-    // FIXME: Caller must set up the overwritting resource
-    it.skip('can reject all resources and overwrites are cleared', async function () {
-      await this.token.rejectAllResources(tokenId);
-
-      expect(await this.token.getResourceOverwrites(tokenId, resId2)).to.eql(BigNumber.from(0));
-    });
-
     it('can reject allresources if approved', async function () {
       await this.token.connect(tokenOwner).approveForResources(approved.address, tokenId);
       await this.token.connect(approved).rejectAllResources(tokenId);
@@ -265,16 +251,6 @@ async function shouldHandleRejectsForResources(
       await this.token.connect(tokenOwner).setApprovalForAllForResources(operator.address, true);
       await this.token.connect(operator).rejectAllResources(tokenId);
 
-      expect(await this.token.getFullResources(tokenId)).to.be.eql([]);
-      expect(await this.token.getFullPendingResources(tokenId)).to.eql([]);
-    });
-
-    // FIXME: Caller must setup all the pending resources
-    it.skip('can reject all pending resources at max capacity', async function () {
-      for (let i = 1; i < 128; i++) {
-        await this.token.addResourceToToken(tokenId, i, 1);
-      }
-      await this.token.connect(tokenOwner).rejectAllResources(tokenId);
       expect(await this.token.getFullResources(tokenId)).to.be.eql([]);
       expect(await this.token.getFullPendingResources(tokenId)).to.eql([]);
     });
@@ -302,6 +278,91 @@ async function shouldHandleRejectsForResources(
       await expect(
         this.token.connect(notApproved).rejectAllResources(tokenId),
       ).to.be.revertedWithCustomError(this.token, 'RMRKNotApprovedForResourcesOrOwner');
+    });
+  });
+}
+
+// Assumes there's a minted token with tokenId with 1 accepted resource and 1 pending to overwrite, and that owner is first address after contract deployer
+async function shouldHandleOverwritesForResources(
+  tokenId: number,
+  addResourceEntryFunc: (token: Contract, data?: string) => Promise<BigNumber>,
+  addResourceToTokenFunc: (
+    token: Contract,
+    tokenId: number,
+    resId: BigNumber,
+    overwrites: BigNumber | number,
+  ) => Promise<void>,
+) {
+  const metaURIDefault = 'metaURI';
+  let tokenOwner: SignerWithAddress;
+
+  before(async () => {
+    const [, ...signersAddr] = await ethers.getSigners();
+    tokenOwner = signersAddr[0];
+  });
+
+  describe('Overwriting resources', async function () {
+    it('can add resource to token overwritting an existing one', async function () {
+      const resId = await addResourceEntryFunc(this.token);
+      const resId2 = await addResourceEntryFunc(this.token);
+
+      await addResourceToTokenFunc(this.token, tokenId, resId, 0);
+      await this.token.connect(tokenOwner).acceptResource(tokenId, 0);
+
+      // Add new resource to overwrite the first, and accept
+      const activeResources = await this.token.getActiveResources(tokenId);
+      await expect(this.token.addResourceToToken(tokenId, resId2, activeResources[0])).to.emit(
+        this.token,
+        'ResourceOverwriteProposed',
+      );
+      const pendingResources = await this.token.getPendingResources(tokenId);
+
+      expect(await this.token.getResourceOverwrites(tokenId, pendingResources[0])).to.eql(
+        activeResources[0],
+      );
+      await expect(this.token.connect(tokenOwner).acceptResource(tokenId, 0)).to.emit(
+        this.token,
+        'ResourceOverwritten',
+      );
+
+      expect(await this.token.getFullResources(tokenId)).to.be.eql([[resId2, metaURIDefault]]);
+      // Overwrite should be gone
+      expect(await this.token.getResourceOverwrites(tokenId, pendingResources[0])).to.eql(
+        BigNumber.from(0),
+      );
+    });
+
+    it('can overwrite non existing resource to token, it could have been deleted', async function () {
+      const resId = await addResourceEntryFunc(this.token);
+
+      await addResourceToTokenFunc(this.token, tokenId, resId, 1);
+      await this.token.connect(tokenOwner).acceptResource(tokenId, 0);
+
+      expect(await this.token.getFullResources(tokenId)).to.be.eql([[resId, metaURIDefault]]);
+    });
+
+    it('can reject resource and overwrites are cleared', async function () {
+      const resId = await addResourceEntryFunc(this.token);
+      await addResourceToTokenFunc(this.token, tokenId, resId, 0);
+      await this.token.connect(tokenOwner).acceptResource(tokenId, 0);
+
+      const resId2 = await addResourceEntryFunc(this.token);
+      await addResourceToTokenFunc(this.token, tokenId, resId2, resId);
+      await this.token.connect(tokenOwner).rejectResource(tokenId, 0);
+
+      expect(await this.token.getResourceOverwrites(tokenId, resId2)).to.eql(BigNumber.from(0));
+    });
+
+    it('can reject all resources and overwrites are cleared', async function () {
+      const resId = await addResourceEntryFunc(this.token);
+      await addResourceToTokenFunc(this.token, tokenId, resId, 0);
+      await this.token.connect(tokenOwner).acceptResource(tokenId, 0);
+
+      const resId2 = await addResourceEntryFunc(this.token);
+      await addResourceToTokenFunc(this.token, tokenId, resId2, resId);
+      await this.token.connect(tokenOwner).rejectAllResources(tokenId);
+
+      expect(await this.token.getResourceOverwrites(tokenId, resId2)).to.eql(BigNumber.from(0));
     });
   });
 }
@@ -386,206 +447,11 @@ async function shouldHandleSetPriorities(tokenId: number) {
   });
 }
 
-async function shouldBehaveLikeMultiResource(
-  mintFunc: (token: Contract, to: string) => Promise<number>,
-  addResourceEntryFunc: (token: Contract, data?: string) => Promise<BigNumber>,
-) {
-  let owner: SignerWithAddress;
-  const metaURIDefault = 'metaURI';
-
-  before(async () => {
-    owner = (await ethers.getSigners())[0];
-  });
-
-  describe('Adding resources to tokens', async function () {
-    it('can add resource to token', async function () {
-      const resId = await addResourceEntryFunc(this.token, 'data1');
-      const resId2 = await addResourceEntryFunc(this.token, 'data2');
-      const tokenId = await mintFunc(this.token, owner.address);
-
-      await expect(this.token.addResourceToToken(tokenId, resId, 0)).to.emit(
-        this.token,
-        'ResourceAddedToToken',
-      );
-      await expect(this.token.addResourceToToken(tokenId, resId2, 0)).to.emit(
-        this.token,
-        'ResourceAddedToToken',
-      );
-
-      const pending = await this.token.getFullPendingResources(tokenId);
-      expect(pending).to.be.eql([
-        [resId, 'data1'],
-        [resId2, 'data2'],
-      ]);
-
-      expect(await this.token.getPendingResObjectByIndex(tokenId, 0)).to.eql([resId, 'data1']);
-    });
-
-    it('cannot add non existing resource to token', async function () {
-      const resId = BigNumber.from(1);
-      const tokenId = await mintFunc(this.token, owner.address);
-
-      await expect(this.token.addResourceToToken(tokenId, resId, 0)).to.be.revertedWithCustomError(
-        this.token,
-        'RMRKNoResourceMatchingId',
-      );
-    });
-
-    it('cannot add resource to non existing token', async function () {
-      const resId = await addResourceEntryFunc(this.token);
-      const tokenId = 1;
-
-      await expect(this.token.addResourceToToken(tokenId, resId, 0)).to.be.revertedWithCustomError(
-        this.token,
-        'ERC721InvalidTokenId',
-      );
-    });
-
-    it('cannot add resource twice to the same token', async function () {
-      const resId = await addResourceEntryFunc(this.token);
-      const tokenId = await mintFunc(this.token, owner.address);
-
-      await this.token.addResourceToToken(tokenId, resId, 0);
-      await expect(this.token.addResourceToToken(tokenId, resId, 0)).to.be.revertedWithCustomError(
-        this.token,
-        'RMRKResourceAlreadyExists',
-      );
-    });
-
-    it('cannot add too many resources to the same token', async function () {
-      const tokenId = await mintFunc(this.token, owner.address);
-
-      for (let i = 1; i <= 128; i++) {
-        const resId = await addResourceEntryFunc(this.token);
-        await this.token.addResourceToToken(tokenId, resId, 0);
-      }
-
-      // Now it's full, next should fail
-      const resId = await addResourceEntryFunc(this.token);
-      await expect(this.token.addResourceToToken(tokenId, resId, 0)).to.be.revertedWithCustomError(
-        this.token,
-        'RMRKMaxPendingResourcesReached',
-      );
-    });
-
-    it('can add same resource to 2 different tokens', async function () {
-      const resId = await addResourceEntryFunc(this.token);
-      const tokenId1 = await mintFunc(this.token, owner.address);
-      const tokenId2 = await mintFunc(this.token, owner.address);
-
-      await this.token.addResourceToToken(tokenId1, resId, 0);
-      await this.token.addResourceToToken(tokenId2, resId, 0);
-
-      expect(await this.token.getPendingResources(tokenId1)).to.be.eql([resId]);
-      expect(await this.token.getPendingResources(tokenId2)).to.be.eql([resId]);
-    });
-  });
-
-  describe('Overwriting resources', async function () {
-    it('can add resource to token overwritting an existing one', async function () {
-      const resId = await addResourceEntryFunc(this.token);
-      const resId2 = await addResourceEntryFunc(this.token);
-      const tokenId = await mintFunc(this.token, owner.address);
-
-      await this.token.addResourceToToken(tokenId, resId, 0);
-      await this.token.acceptResource(tokenId, 0);
-
-      // Add new resource to overwrite the first, and accept
-      const activeResources = await this.token.getActiveResources(tokenId);
-      await expect(this.token.addResourceToToken(tokenId, resId2, activeResources[0])).to.emit(
-        this.token,
-        'ResourceOverwriteProposed',
-      );
-      const pendingResources = await this.token.getPendingResources(tokenId);
-
-      expect(await this.token.getResourceOverwrites(tokenId, pendingResources[0])).to.eql(
-        activeResources[0],
-      );
-      await expect(this.token.acceptResource(tokenId, 0)).to.emit(
-        this.token,
-        'ResourceOverwritten',
-      );
-
-      expect(await this.token.getFullResources(tokenId)).to.be.eql([[resId2, metaURIDefault]]);
-      // Overwrite should be gone
-      expect(await this.token.getResourceOverwrites(tokenId, pendingResources[0])).to.eql(
-        BigNumber.from(0),
-      );
-    });
-
-    it('can overwrite non existing resource to token, it could have been deleted', async function () {
-      const resId = await addResourceEntryFunc(this.token);
-      const tokenId = await mintFunc(this.token, owner.address);
-
-      await this.token.addResourceToToken(tokenId, resId, 1);
-      await this.token.acceptResource(tokenId, 0);
-
-      expect(await this.token.getFullResources(tokenId)).to.be.eql([[resId, metaURIDefault]]);
-    });
-  });
-
-  describe('Token URI', async function () {
-    it('can set fallback URI', async function () {
-      await this.token.setFallbackURI('TestURI');
-      expect(await this.token.getFallbackURI()).to.be.eql('TestURI');
-    });
-
-    it('gets fallback URI if no active resources on token', async function () {
-      const fallBackUri = 'fallback404';
-      const tokenId = await mintFunc(this.token, owner.address);
-
-      await this.token.setFallbackURI(fallBackUri);
-      expect(await this.token.tokenURI(tokenId)).to.eql(fallBackUri);
-    });
-
-    it('can get token URI when resource is not enumerated', async function () {
-      const resId = await addResourceEntryFunc(this.token);
-      const resId2 = await addResourceEntryFunc(this.token);
-      const tokenId = await mintFunc(this.token, owner.address);
-
-      await this.token.addResourceToToken(tokenId, resId, 0);
-      await this.token.addResourceToToken(tokenId, resId2, 0);
-      await this.token.acceptResource(tokenId, 0);
-      await this.token.acceptResource(tokenId, 0);
-      expect(await this.token.tokenURI(tokenId)).to.eql(metaURIDefault);
-    });
-
-    it('can get token URI when resource is enumerated', async function () {
-      const resId = await addResourceEntryFunc(this.token);
-      const resId2 = await addResourceEntryFunc(this.token);
-      const tokenId = await mintFunc(this.token, owner.address);
-
-      await this.token.addResourceToToken(tokenId, resId, 0);
-      await this.token.addResourceToToken(tokenId, resId2, 0);
-      await this.token.acceptResource(tokenId, 0);
-      await this.token.acceptResource(tokenId, 0);
-      await this.token.setTokenEnumeratedResource(resId, true);
-      expect(await this.token.isTokenEnumeratedResource(resId)).to.eql(true);
-      expect(await this.token.tokenURI(tokenId)).to.eql(`${metaURIDefault}${tokenId}`);
-    });
-
-    it('can get token URI at specific index', async function () {
-      const resId = BigNumber.from(1);
-      const resId2 = BigNumber.from(2);
-      const tokenId = await mintFunc(this.token, owner.address);
-
-      await this.token.addResourceEntry(resId, 'UriA');
-      await this.token.addResourceEntry(resId2, 'UriB');
-      await this.token.addResourceToToken(tokenId, resId, 0);
-      await this.token.addResourceToToken(tokenId, resId2, 0);
-      await this.token.acceptResource(tokenId, 0);
-      await this.token.acceptResource(tokenId, 0);
-
-      expect(await this.token.tokenURIAtIndex(tokenId, 1)).to.eql('UriB');
-    });
-  });
-}
-
 export {
-  shouldBehaveLikeMultiResource,
-  shouldSupportInterfaces,
-  shouldHandleApprovalsForResources,
   shouldHandleAcceptsForResources,
+  shouldHandleApprovalsForResources,
+  shouldHandleOverwritesForResources,
   shouldHandleRejectsForResources,
   shouldHandleSetPriorities,
+  shouldSupportInterfacesForResources,
 };
