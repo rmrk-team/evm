725620e904207b9506513255998a4d50f8ae8141
steven2308 <steven2308@outlook.com>
2022-08-26 10:55:02 +0300

    Removes MultiResourceAbstract.

--- contracts/RMRK/RMRKEquippableWithNesting.sol
+++ contracts/RMRK/RMRKEquippableWithNesting.sol
@@ -8,16 +8,29 @@
 
 pragma solidity ^0.8.15;
 
-import "./abstracts/MultiResourceAbstract.sol";
 import "./interfaces/IRMRKBaseStorage.sol";
 import "./interfaces/IRMRKEquippableWithNesting.sol";
 import "./interfaces/IRMRKNesting.sol";
 import "./interfaces/IRMRKNestingWithEquippable.sol";
 import "./library/RMRKLib.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 import "hardhat/console.sol";
 
+// MultiResource
+error RMRKBadPriorityListLength();
+error RMRKIndexOutOfRange();
+error RMRKMaxPendingResourcesReached();
+error RMRKNoResourceMatchingId();
+error RMRKResourceAlreadyExists();
+error RMRKWriteToZero();
+error RMRKNotApprovedForResourcesOrOwner();
+error RMRKApprovalForResourcesToCurrentOwner();
+error RMRKApproveForResourcesCallerIsNotOwnerNorApprovedForAll();
+error RMRKApproveForResourcesToCaller();
+
+error ERC721InvalidTokenId();
 error ERC721NotApprovedOrOwner();
 error RMRKBaseRequiredForParts();
 error RMRKTokenCannotBeEquippedWithResourceIntoSlot();
@@ -27,7 +40,7 @@ error RMRKNotEquipped();
 error RMRKSlotAlreadyUsed();
 error RMRKTokenDoesNotHaveActiveResource();
 
-contract RMRKEquippableWithNesting is IRMRKEquippableWithNesting, MultiResourceAbstract {
+contract RMRKEquippableWithNesting is Context, IRMRKEquippableWithNesting {
 
     using RMRKLib for uint64[];
     using RMRKLib for uint128[];
@@ -62,9 +75,35 @@ contract RMRKEquippableWithNesting is IRMRKEquippableWithNesting, MultiResourceA
         string metadataURI; //n bytes 32+
     }
 
-    constructor(address nestingAddress) {
-        _setNestingAddress(nestingAddress);
-    }
+    // ------------------- RESOURCES --------------
+    //mapping of uint64 Ids to resource object
+    mapping(uint64 => string) internal _resources;
+
+    //mapping of tokenId to new resource, to resource to be replaced
+    mapping(uint256 => mapping(uint64 => uint64)) internal _resourceOverwrites;
+
+    //mapping of tokenId to all resources
+    mapping(uint256 => uint64[]) internal _activeResources;
+
+    //mapping of tokenId to an array of resource priorities
+    mapping(uint256 => uint16[]) internal _activeResourcePriorities;
+
+    //Double mapping of tokenId to active resources
+    mapping(uint256 => mapping(uint64 => bool)) internal _tokenResources;
+
+    //mapping of tokenId to all resources by priority
+    mapping(uint256 => uint64[]) internal _pendingResources;
+
+    //List of all resources
+    uint64[] internal _allResources;
+
+    // Mapping from token ID to approved address for resources
+    mapping(uint256 => address) internal _tokenApprovalsForResources;
+
+    // Mapping from owner to operator approvals for resources
+    mapping(address => mapping(address => bool)) internal _operatorApprovalsForResources;
+
+    // ------------------- Equippable --------------
 
     address private _nestingAddress;
 
@@ -84,10 +123,6 @@ contract RMRKEquippableWithNesting is IRMRKEquippableWithNesting, MultiResourceA
 
     //Mapping of refId to parent contract address and valid slotId
     mapping(uint64 => mapping(address => uint64)) private _validParentSlots;
-
-    function ownerOf(uint tokenId) internal view returns(address) {
-        return IRMRKNesting(_nestingAddress).ownerOf(tokenId);
-    }
 
     function _onlyApprovedOrOwner(uint tokenId) internal view {
         if (!IRMRKNestingWithEquippable(_nestingAddress).isApprovedOrOwner(_msgSender(), tokenId))
@@ -114,6 +149,22 @@ contract RMRKEquippableWithNesting is IRMRKEquippableWithNesting, MultiResourceA
         _;
     }
 
+    constructor(address nestingAddress) {
+        _setNestingAddress(nestingAddress);
+    }
+
+    /**
+     * @dev See {IERC165-supportsInterface}.
+     */
+    function supportsInterface(bytes4 interfaceId) public virtual view returns (bool) {
+        return (
+            interfaceId == type(IRMRKEquippableWithNesting).interfaceId ||
+            interfaceId == type(IRMRKEquippable).interfaceId ||
+            interfaceId == type(IRMRKMultiResource).interfaceId ||
+            interfaceId == type(IERC165).interfaceId
+        );
+    }
+
     function _setNestingAddress(address nestingAddress) internal {
         address oldAddress = _nestingAddress;
         _nestingAddress = nestingAddress;
@@ -124,15 +175,290 @@ contract RMRKEquippableWithNesting is IRMRKEquippableWithNesting, MultiResourceA
         return _nestingAddress;
     }
 
-    function supportsInterface(bytes4 interfaceId) public virtual view returns (bool) {
-        console.logBytes4(type(IRMRKEquippable).interfaceId);
-        return (
-            interfaceId == type(IRMRKEquippableWithNesting).interfaceId ||
-            interfaceId == type(IRMRKEquippable).interfaceId ||
-            interfaceId == type(IRMRKMultiResource).interfaceId ||
-            interfaceId == type(IERC165).interfaceId
-        );
+    // ------------------------------- RESOURCES ------------------------------
+
+    // --------------------------- GETTING RESOURCES --------------------------
+
+    function getResource(
+        uint64 resourceId
+    ) public view virtual returns (Resource memory)
+    {
+        string memory resourceData = _resources[resourceId];
+        if(bytes(resourceData).length == 0)
+            revert RMRKNoResourceMatchingId();
+        Resource memory resource = Resource({
+            id: resourceId,
+            metadataURI: resourceData
+        });
+        return resource;
     }
+
+    function getAllResources() public view virtual returns (uint64[] memory) {
+        return _allResources;
+    }
+
+    function getResObjectByIndex(
+        uint256 tokenId,
+        uint256 index
+    ) external view virtual returns(Resource memory) {
+        uint64 resourceId = getActiveResources(tokenId)[index];
+        return getResource(resourceId);
+    }
+
+    function getPendingResObjectByIndex(
+        uint256 tokenId,
+        uint256 index
+    ) external view virtual returns(Resource memory) {
+        uint64 resourceId = getPendingResources(tokenId)[index];
+        return getResource(resourceId);
+    }
+
+    function getFullResources(
+        uint256 tokenId
+    ) external view virtual returns (Resource[] memory) {
+        uint64[] memory resourceIds = _activeResources[tokenId];
+        return _getResourcesById(resourceIds);
+    }
+
+    function getFullPendingResources(
+        uint256 tokenId
+    ) external view virtual returns (Resource[] memory) {
+        uint64[] memory resourceIds = _pendingResources[tokenId];
+        return _getResourcesById(resourceIds);
+    }
+
+    function _getResourcesById(
+        uint64[] memory resourceIds
+    ) internal view virtual returns (Resource[] memory) {
+        uint256 len = resourceIds.length;
+        Resource[] memory resources = new Resource[](len);
+        for (uint i; i<len;) {
+            resources[i] = getResource(resourceIds[i]);
+            unchecked {++i;}
+        }
+        return resources;
+    }
+
+    function getActiveResources(
+        uint256 tokenId
+    ) public view virtual returns(uint64[] memory) {
+        return _activeResources[tokenId];
+    }
+
+    function getPendingResources(
+        uint256 tokenId
+    ) public view virtual returns(uint64[] memory) {
+        return _pendingResources[tokenId];
+    }
+
+    function getActiveResourcePriorities(
+        uint256 tokenId
+    ) public view virtual returns(uint16[] memory) {
+        return _activeResourcePriorities[tokenId];
+    }
+
+    function getResourceOverwrites(
+        uint256 tokenId,
+        uint64 resourceId
+    ) public view virtual returns(uint64) {
+        return _resourceOverwrites[tokenId][resourceId];
+    }
+
+    // --------------------------- HANDLING RESOURCES -------------------------
+
+    function acceptResource(
+        uint256 tokenId,
+        uint256 index
+    ) external virtual onlyApprovedForResourcesOrOwner(tokenId) {
+        _acceptResource(tokenId, index);
+    }
+
+    function rejectResource(
+        uint256 tokenId,
+        uint256 index
+    ) external virtual onlyApprovedForResourcesOrOwner(tokenId) {
+        _rejectResource(tokenId, index);
+    }
+
+    function rejectAllResources(
+        uint256 tokenId
+    ) external virtual onlyApprovedForResourcesOrOwner(tokenId) {
+        _rejectAllResources(tokenId);
+    }
+
+    function setPriority(
+        uint256 tokenId,
+        uint16[] memory priorities
+    ) external virtual onlyApprovedForResourcesOrOwner(tokenId) {
+        _setPriority(tokenId, priorities);
+    }
+
+    function _acceptResource(uint256 tokenId, uint256 index) internal {
+        if(index >= _pendingResources[tokenId].length) revert RMRKIndexOutOfRange();
+        uint64 resourceId = _pendingResources[tokenId][index];
+        _pendingResources[tokenId].removeItemByIndex(index);
+
+        uint64 overwrite = _resourceOverwrites[tokenId][resourceId];
+        if (overwrite != uint64(0)) {
+            // We could check here that the resource to overwrite actually exists but it is probably harmless.
+            _activeResources[tokenId].removeItemByValue(overwrite);
+            emit ResourceOverwritten(tokenId, overwrite, resourceId);
+            delete(_resourceOverwrites[tokenId][resourceId]);
+        }
+        _activeResources[tokenId].push(resourceId);
+        //Push 0 value of uint16 to array, e.g., uninitialized
+        _activeResourcePriorities[tokenId].push(uint16(0));
+        emit ResourceAccepted(tokenId, resourceId);
+    }
+
+    function _rejectResource(uint256 tokenId, uint256 index) internal {
+        if(index >= _pendingResources[tokenId].length) revert RMRKIndexOutOfRange();
+        uint64 resourceId = _pendingResources[tokenId][index];
+        _pendingResources[tokenId].removeItemByIndex(index);
+        _tokenResources[tokenId][resourceId] = false;
+        delete(_resourceOverwrites[tokenId][resourceId]);
+
+        emit ResourceRejected(tokenId, resourceId);
+    }
+
+    function _rejectAllResources(uint256 tokenId) internal {
+        uint256 len = _pendingResources[tokenId].length;
+        for (uint i; i<len;) {
+            uint64 resourceId = _pendingResources[tokenId][i];
+            delete _resourceOverwrites[tokenId][resourceId];
+            unchecked {++i;}
+        }
+
+        delete(_pendingResources[tokenId]);
+        emit ResourceRejected(tokenId, uint64(0));
+    }
+
+    function _setPriority(
+        uint256 tokenId,
+        uint16[] memory priorities
+    ) internal {
+        uint256 length = priorities.length;
+        if(length != _activeResources[tokenId].length) revert RMRKBadPriorityListLength();
+        _activeResourcePriorities[tokenId] = priorities;
+
+        emit ResourcePrioritySet(tokenId);
+    }
+
+    // This is expected to be implemented with custom guard:
+    function _addResourceToToken(
+        uint256 tokenId,
+        uint64 resourceId,
+        uint64 overwrites
+    ) internal {
+        if(_tokenResources[tokenId][resourceId])
+            revert RMRKResourceAlreadyExists();
+
+        if(bytes(_resources[resourceId]).length == 0)
+            revert RMRKNoResourceMatchingId();
+
+        if(_pendingResources[tokenId].length >= 128)
+            revert RMRKMaxPendingResourcesReached();
+
+        _tokenResources[tokenId][resourceId] = true;
+
+        _pendingResources[tokenId].push(resourceId);
+
+        if (overwrites != uint64(0)) {
+            _resourceOverwrites[tokenId][resourceId] = overwrites;
+            emit ResourceOverwriteProposed(tokenId, resourceId, overwrites);
+        }
+
+        emit ResourceAddedToToken(tokenId, resourceId);
+    }
+
+    // ----------------------------- TOKEN URI --------------------------------
+    /**
+     * @dev See {IERC721Metadata-tokenURI}. Overwritten for MR
+     */
+    function tokenURI(
+        uint256 tokenId
+    ) public view virtual returns (string memory) {
+        return _tokenURIAtIndex(tokenId, 0);
+    }
+
+    /**
+     * @dev Base URI for computing {tokenURI}. If set, the resulting URI for each
+     * token will be the concatenation of the `baseURI` and the `tokenId`. Empty
+     * by default, can be overridden in child contracts.
+     */
+    function _baseURI() internal view virtual returns (string memory) {
+        return "";
+    }
+
+    function tokenURIAtIndex(
+        uint256 tokenId,
+        uint256 index
+    ) public view virtual returns (string memory) {
+        return _tokenURIAtIndex(tokenId, index);
+    }
+
+    function _tokenURIAtIndex(
+        uint256 tokenId,
+        uint256 index
+    ) internal virtual view returns (string memory) {
+        _requireMinted(tokenId);
+        // TODO: Discuss is this is the best default path.
+        // We could return empty string so it returns something if a token has no resources, but it might hide erros
+        if (!(index < _activeResources[tokenId].length))
+            revert RMRKIndexOutOfRange();
+
+        uint64 activeResId = _activeResources[tokenId][index];
+        Resource memory _activeRes = getResource(activeResId);
+        string memory uri = string(
+            abi.encodePacked(_baseURI(), _activeRes.metadataURI)
+        );
+
+        return uri;
+    }
+
+    // ----------------------- APPROVALS FOR RESOURCES ------------------------
+
+    function approveForResources(address to, uint256 tokenId) external virtual {
+        address owner = ownerOf(tokenId);
+        if(to == owner)
+            revert RMRKApprovalForResourcesToCurrentOwner();
+
+        // We want to bypass the check if the caller is the linked nesting contract and it's simply removing approvals
+        bool isNestingCallToRemoveApprovals = (_msgSender() == _nestingAddress &&  to == address(0));
+
+        if(!isNestingCallToRemoveApprovals && _msgSender() != owner && !isApprovedForAllForResources(owner, _msgSender()))
+            revert RMRKApproveForResourcesCallerIsNotOwnerNorApprovedForAll();
+        _approveForResources(to, tokenId);
+    }
+
+    function getApprovedForResources(uint256 tokenId) public virtual view returns (address) {
+        _requireMinted(tokenId);
+        return _tokenApprovalsForResources[tokenId];
+    }
+
+    function setApprovalForAllForResources(address operator, bool approved) external virtual {
+        address owner = _msgSender();
+        if(owner == operator)
+            revert RMRKApproveForResourcesToCaller();
+
+        _operatorApprovalsForResources[owner][operator] = approved;
+        emit ApprovalForAllForResources(owner, operator, approved);
+    }
+
+    function isApprovedForAllForResources(address owner, address operator) public virtual view returns (bool) {
+        return _operatorApprovalsForResources[owner][operator];
+    }
+
+    function _approveForResources(address to, uint256 tokenId) internal virtual {
+        _tokenApprovalsForResources[tokenId] = to;
+        emit ApprovalForResources(ownerOf(tokenId), to, tokenId);
+    }
+
+    function _cleanApprovals(address owner, uint256 tokenId) internal virtual {
+        _approveForResources(owner, tokenId);
+    }
+
+    // ------------------------------- EQUIPPING ------------------------------
 
     function equip(
         uint256 tokenId,
@@ -363,37 +689,6 @@ contract RMRKEquippableWithNesting is IRMRKEquippableWithNesting, MultiResourceA
     }
 
     ////////////////////////////////////////
-    //                RESOURCES
-    ////////////////////////////////////////
-
-    function acceptResource(
-        uint256 tokenId,
-        uint256 index
-    ) external virtual onlyApprovedForResourcesOrOwner(tokenId) {
-        _acceptResource(tokenId, index);
-    }
-
-    function rejectResource(
-        uint256 tokenId,
-        uint256 index
-    ) external virtual onlyApprovedForResourcesOrOwner(tokenId) {
-        _rejectResource(tokenId, index);
-    }
-
-    function rejectAllResources(
-        uint256 tokenId
-    ) external virtual onlyApprovedForResourcesOrOwner(tokenId) {
-        _rejectAllResources(tokenId);
-    }
-
-    function setPriority(
-        uint256 tokenId,
-        uint16[] memory priorities
-    ) external virtual onlyApprovedForResourcesOrOwner(tokenId) {
-        _setPriority(tokenId, priorities);
-    }
-
-    ////////////////////////////////////////
     //       MANAGING EXTENDED RESOURCES
     ////////////////////////////////////////
 
@@ -402,15 +697,24 @@ contract RMRKEquippableWithNesting is IRMRKEquippableWithNesting, MultiResourceA
         uint64[] memory fixedPartIds,
         uint64[] memory slotPartIds
     ) internal {
+        uint64 id = resource.id;
+
+        if(id == uint64(0))
+            revert RMRKWriteToZero();
+        if(bytes(_resources[id]).length > 0)
+            revert RMRKResourceAlreadyExists();
         if (resource.baseAddress == address(0) && (fixedPartIds.length > 0 || slotPartIds.length > 0))
             revert RMRKBaseRequiredForParts();
 
-        _addResourceEntry(resource.id, resource.metadataURI);
+        _resources[id] = resource.metadataURI;
+        _allResources.push(id);
 
         _baseAddresses[resource.id] = resource.baseAddress;
         _equippableRefIds[resource.id] = resource.equippableRefId;
         _fixedPartIds[resource.id] = fixedPartIds;
         _slotPartIds[resource.id] = slotPartIds;
+
+        emit ResourceSet(id);
     }
 
     function getExtendedResource(
@@ -470,29 +774,25 @@ contract RMRKEquippableWithNesting is IRMRKEquippableWithNesting, MultiResourceA
         return extendedResources;
     }
 
-    // Approvals
+    ////////////////////////////////////////
+    //              UTILS
+    ////////////////////////////////////////
 
-    function approveForResources(address to, uint256 tokenId) external virtual {
-        address owner = ownerOf(tokenId);
-        if(to == owner)
-            revert RMRKApprovalForResourcesToCurrentOwner();
-
-        // We want to bypass the check if the caller is the linked nesting contract and it's simply removing approvals
-        bool isNestingCallToRemoveApprovals = (_msgSender() == _nestingAddress &&  to == address(0));
-
-        if(!isNestingCallToRemoveApprovals && _msgSender() != owner && !isApprovedForAllForResources(owner, _msgSender()))
-            revert RMRKApproveForResourcesCallerIsNotOwnerNorApprovedForAll();
-        _approveForResources(owner, to, tokenId);
+    /**
+     * @dev Reverts if the `tokenId` has not been minted yet.
+     */
+    function _requireMinted(uint256 tokenId) internal view virtual {
+        // FIXME: error is not consistent (others use ERC721InvalidTokenId)
+        if(!_exists(tokenId))
+            revert ERC721InvalidTokenId();
     }
 
-    function setApprovalForAllForResources(address operator, bool approved) external virtual {
-        address owner = _msgSender();
-        if(owner == operator)
-            revert RMRKApproveForResourcesToCaller();
-        _setApprovalForAllForResources(owner, operator, approved);
-    }
-
-    function _exists(uint256 tokenId) internal override view virtual returns (bool) {
+    function _exists(uint256 tokenId) internal view virtual returns (bool) {
         return ownerOf(tokenId) != address(0);
+    }
+
+
+    function ownerOf(uint tokenId) internal view returns(address) {
+        return IRMRKNesting(_nestingAddress).ownerOf(tokenId);
     }
 }
