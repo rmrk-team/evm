1a39b42c12e111d410f941bc319d240ef6f395d5
steven2308 <steven2308@outlook.com>
2022-08-27 10:10:09 +0300

    Adds RMRKEquippable merged implementation, too big to compile yet.

---
+++ contracts/implementations/RMRKEquippableImpl.sol
@@ -0,0 +1,124 @@
+// SPDX-License-Identifier: Apache-2.0
+
+pragma solidity ^0.8.16;
+
+import "../RMRK/utils/RMRKMintingUtils.sol";
+import "../RMRK/RMRKEquippable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+/* import "hardhat/console.sol"; */
+
+error RMRKMintUnderpriced();
+error RMRKMintZero();
+
+
+contract RMRKEquippableImpl is Ownable, RMRKMintingUtils, RMRKEquippable {
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 maxSupply,
+        uint256 pricePerMint
+    )
+    RMRKEquippable(name, symbol)
+    RMRKMintingUtils(maxSupply, pricePerMint)
+    {}
+
+    function safeMint(address to, uint256 tokenId) public {
+        _safeMint(to, tokenId);
+    }
+
+    function safeMint(
+        address to,
+        uint256 tokenId,
+        bytes memory _data
+    ) public {
+        _safeMint(to, tokenId, _data);
+    }
+
+    /*
+    Template minting logic
+    */
+    function mint(address to, uint256 numToMint) external payable saleIsOpen {
+        (uint nextToken, uint totalSupplyOffset) = _preMint(numToMint);
+
+        for(uint i = nextToken; i < totalSupplyOffset;) {
+            _safeMint(to, i);
+            unchecked {++i;}
+        }
+    }
+
+    /*
+    Template minting logic
+    */
+    function mintNesting(address to, uint256 numToMint, uint256 destinationId) external payable saleIsOpen {
+        (uint nextToken, uint totalSupplyOffset) = _preMint(numToMint);
+
+        for(uint i = nextToken; i < totalSupplyOffset;) {
+            _nestMint(to, i, destinationId);
+            unchecked {++i;}
+        }
+    }
+
+    function _preMint(uint256 numToMint) private returns(uint, uint) {
+        if (numToMint == uint256(0)) revert RMRKMintZero();
+        if (numToMint + _totalSupply > _maxSupply) revert RMRKMintOverMax();
+
+        uint256 mintPriceRequired = numToMint * _pricePerMint;
+        if (mintPriceRequired != msg.value)
+            revert RMRKMintUnderpriced();
+
+        uint256 nextToken = _totalSupply+1;
+        _totalSupply += numToMint;
+        uint256 totalSupplyOffset = _totalSupply+1;
+
+        return (nextToken, totalSupplyOffset);
+    }
+
+    //update for reentrancy
+    function burn(uint256 tokenId) public onlyApprovedOrDirectOwner(tokenId) {
+        _burn(tokenId);
+    }
+
+    // Utility transfers:
+
+    function transfer(
+        address to,
+        uint256 tokenId
+    ) public virtual {
+        transferFrom(_msgSender(), to, tokenId);
+    }
+
+    function nestTransfer(
+        address to,
+        uint256 tokenId,
+        uint256 destinationId
+    ) public virtual {
+        nestTransferFrom(_msgSender(), to, tokenId, destinationId);
+    }
+
+    function addResourceToToken(
+        uint256 tokenId,
+        uint64 resourceId,
+        uint64 overwrites
+    ) external {
+        // This reverts if token does not exist:
+        ownerOf(tokenId);
+        _addResourceToToken(tokenId, resourceId, overwrites);
+    }
+
+    function addResourceEntry(
+        ExtendedResource calldata resource,
+        uint64[] calldata fixedPartIds,
+        uint64[] calldata slotPartIds
+    ) external {
+        _addResourceEntry(resource, fixedPartIds, slotPartIds);
+    }
+
+    function setValidParentRefId(
+        uint64 refId,
+        address parentAddress,
+        uint64 partId
+    ) external {
+        _setValidParentRefId(refId, parentAddress, partId);
+    }
+}
