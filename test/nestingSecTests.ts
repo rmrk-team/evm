3a0ba5239e4e2e1704ddac6ce538e6550d2b3c29
CicadaNCR <henr.tosca@gmail.com>
2022-08-30 09:10:09 -0500

    rename sec test contract

--- test/nestingSecTests.ts
+++ test/nestingSecTests.ts
@@ -6,7 +6,7 @@ import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
 
 describe('NestingMock', function () {
   let parent: Contract;
-  let spammer: Contract;
+  let adder: Contract;
   let owner: SignerWithAddress;
 
   async function nestingFixture() {
@@ -14,23 +14,23 @@ describe('NestingMock', function () {
     parent = await NestingFactory.deploy('Test', 'TST');
     await parent.deployed();
 
-    const ChildSpammer = await ethers.getContractFactory('ChildSpammer');
-    spammer = await ChildSpammer.deploy();
-    await spammer.deployed();
+    const Childadder = await ethers.getContractFactory('Childadder');
+    adder = await Childadder.deploy();
+    await adder.deployed();
 
-    return { parent, spammer };
+    return { parent, adder };
   }
 
   beforeEach(async function () {
     owner = (await ethers.getSigners())[0];
-    ({ parent, spammer } = await loadFixture(nestingFixture));
+    ({ parent, adder } = await loadFixture(nestingFixture));
   });
 
-  describe('spam children', async function () {
+  describe('add children', async function () {
     it('Cannot add multiple chilren', async function () {
       await parent.connect(owner).mint(owner.address, 1);
       // Propose 10 children with the same params to the parent token
-      await spammer.spamChild(parent.address, 1, 1, 10);
+      await adder.addChild(parent.address, 1, 1, 10);
 
       await parent.connect(owner).acceptChild(1, 0);
       await expect(parent.connect(owner).acceptChild(1, 0)).to.be.revertedWithCustomError(
