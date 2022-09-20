4c055a0d04b3ac6cdb820d6c19368f60f329cc26
Stevan Bogosavljevic <stevyhacker@gmail.com>
2022-09-20 14:04:40 +0200

    Add royalties

---
+++ contracts/RMRK/interfaces/RMRKRoyalties.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/interfaces/IERC2981.sol";
+
+
+abstract contract RMRKRoyalties is IERC2981 {
+
+    //@notice Royalty details
+    address _royaltyRecipient;
+    uint256 _royaltyPercentageBps;
+
+    constructor(
+        address royaltyRecipient,
+        uint256 royaltyPercentageBps //in basis points
+    ) {
+        _royaltyRecipient = royaltyRecipient;
+        _royaltyPercentageBps = royaltyPercentageBps;
+    }
+
+    //@notice Requires access control on the implementation contract like implementing Ownable and setting onlyOwner modifier
+    function updateRoyaltyRecipient(address newRoyaltyRecipient) virtual external;
+
+    //@param tokenId - the token id to get the royalty info for
+    //@param salePrice - the sale price of the NFT
+    function royaltyInfo(uint256 tokenId, uint256 salePrice)
+    external
+    view
+    override
+    returns (address receiver, uint256 royaltyAmount)
+    {
+        receiver = _royaltyRecipient;
+        royaltyAmount = (salePrice * _royaltyPercentageBps) / 10000;
+    }
+}
