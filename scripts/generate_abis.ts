ea8b13fd3a78858f0addadf87b39e987eadf08fc
Yuri <yuripetusko@gmail.com>
2022-09-20 21:50:26 +0200

    Simple script to generate abis jsons from implementation contracts

---
+++ scripts/generate_abis.ts
@@ -0,0 +1,37 @@
+import fs from 'fs';
+import walkSync from 'walk-sync';
+
+const getTheAbi = () => {
+  try {
+    const implementations = walkSync(`${process.cwd()}/artifacts/contracts/implementations`, {
+      directories: false,
+    });
+
+    const dirExists = fs.existsSync(`${process.cwd()}/artifacts/abis/implementations`);
+    if (!dirExists) {
+      fs.mkdirSync(`${process.cwd()}/artifacts/abis`);
+      fs.mkdirSync(`${process.cwd()}/artifacts/abis/implementations`);
+    }
+
+    implementations.forEach((implementation) => {
+      const filename = implementation.slice(0, implementation.indexOf('.sol'));
+      const file = fs.readFileSync(
+        `${process.cwd()}/artifacts/contracts/implementations/${implementation}`,
+        'utf8',
+      );
+      const json = JSON.parse(file);
+
+      if (json.abi) {
+        fs.writeFileSync(
+          `${process.cwd()}/artifacts/abis/implementations/${filename}.json`,
+          JSON.stringify(json.abi),
+        );
+      }
+
+    });
+  } catch (e) {
+    console.log(`e`, e);
+  }
+};
+
+getTheAbi();
