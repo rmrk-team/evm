c9847148a6223c48153d2af44344476a74f0d4b7
Jan Turk <jan.turk12@gmail.com>
2022-10-22 00:08:13 +0200

    Fully document core smart contracts with NatSpec
    NatSpec comments have been added to the core smart contracts, the
    existing ones updated and some reclassified.

--- contracts/RMRK/core/IRMRKCore.sol
+++ contracts/RMRK/core/IRMRKCore.sol
@@ -2,19 +2,28 @@
 
 pragma solidity ^0.8.0;
 
+/**
+ * @title IRMRKCore
+ * @author RMRK team
+ * @notice Interface smart contract for RMRK core module.
+ */
 interface IRMRKCore {
     /**
-     * @dev Returns the token collection name.
+     * @notice Used to retrieve the collection name.
+     * @return string Name of the collection
      */
     function name() external view returns (string memory);
 
     /**
-     * @dev Returns the token collection symbol.
+     * @notice Used to retrieve the collection symbol.
+     * @return string Symbol of the collection
      */
     function symbol() external view returns (string memory);
 
     /**
-     * @dev See {IERC721Metadata-tokenURI}.
+     * @notice Used to retrieve the metadata URI of a token.
+     * @param tokenId ID of the token to retrieve the metadata URI for
+     * @return string Metadata URI of the specified token
      */
     function tokenURI(uint256 tokenId) external view returns (string memory);
 }
--- contracts/RMRK/core/RMRKCore.sol
+++ contracts/RMRK/core/RMRKCore.sol
@@ -5,10 +5,13 @@ pragma solidity ^0.8.15;
 import "./IRMRKCore.sol";
 
 /**
- * @dev This is currently just a passthrough contract which allows for granular editing of
- * base-level ERC721 functions,
+ * @title RMRKCore
+ * @author RMRK team
+ * @notice Smart contract of the RMRK core module.
+ * @dev This is currently just a passthrough contract which allows for granular editing of base-level ERC721 functions.
  */
 contract RMRKCore is IRMRKCore {
+    /// @notice Version of the @rmrk-team/evm-contracts package
     string public constant VERSION = "0.12.0";
 
     constructor(string memory name_, string memory symbol_) {
@@ -16,27 +19,34 @@ contract RMRKCore is IRMRKCore {
         _symbol = symbol_;
     }
 
-    // Token name
+    /// Token name
     string private _name;
 
-    // Token symbol
+    /// Token symbol
     string private _symbol;
 
     /**
-     * @dev See {IERC721Metadata-name}.
+     * @notice Used to retrieve the collection name.
+     * @return string Name of the collection
      */
     function name() public view virtual override returns (string memory) {
         return _name;
     }
 
     /**
-     * @dev See {IERC721Metadata-symbol}.
+     * @notice Used to retrieve the collection symbol.
+     * @return string Symbol of the collection
      */
     function symbol() public view virtual override returns (string memory) {
         return _symbol;
     }
 
-    function tokenURI(uint256)
+    /**
+     * @notice Used to retrieve the metadata URI of a token.
+     * @param tokenId ID of the token to retrieve the metadata URI for
+     * @return string Metadata URI of the specified token
+     */
+    function tokenURI(uint256 tokenId)
         public
         view
         virtual
@@ -47,18 +57,18 @@ contract RMRKCore is IRMRKCore {
     }
 
     /**
-     * @dev Hook that is called before any token transfer. This includes minting
-     * and burning.
+     * @notice Hook that is called before any token transfer. This includes minting and burning.
+     * @dev Calling conditions:
      *
-     * Calling conditions:
+     *  - When `from` and `to` are both non-zero, ``from``'s `tokenId` will be transferred to `to`.
+     *  - When `from` is zero, `tokenId` will be minted to `to`.
+     *  - When `to` is zero, ``from``'s `tokenId` will be burned.
+     *  - `from` and `to` are never zero at the same time.
      *
-     * - When `from` and `to` are both non-zero, ``from``'s `tokenId` will be
-     * transferred to `to`.
-     * - When `from` is zero, `tokenId` will be minted for `to`.
-     * - When `to` is zero, ``from``'s `tokenId` will be burned.
-     * - `from` and `to` are never both zero.
-     *
-     * To learn more about hooks, head to xref:ROOT:extending-contracts.adoc#using-hooks[Using Hooks].
+     *  To learn more about hooks, head to xref:ROOT:extending-contracts.adoc#using-hooks[Using Hooks].
+     * @param from Address from which the token is being transferred
+     * @param to Address to which the token is being transferred
+     * @param tokenId ID of the token being transferred
      */
     function _beforeTokenTransfer(
         address from,
@@ -67,15 +77,16 @@ contract RMRKCore is IRMRKCore {
     ) internal virtual {}
 
     /**
-     * @dev Hook that is called after any transfer of tokens. This includes
-     * minting and burning.
+     * @notice Hook that is called after any transfer of tokens. This includes minting and burning.
+     * @dev Calling conditions:
      *
-     * Calling conditions:
+     *  - When `from` and `to` are both non-zero.
+     *  - `from` and `to` are never zero at the same time.
      *
-     * - when `from` and `to` are both non-zero.
-     * - `from` and `to` are never both zero.
-     *
-     * To learn more about hooks, head to xref:ROOT:extending-contracts.adoc#using-hooks[Using Hooks].
+     *  To learn more about hooks, head to xref:ROOT:extending-contracts.adoc#using-hooks[Using Hooks].
+     * @param from Address from which the token has been transferred
+     * @param to Address to which the token has been transferred
+     * @param tokenId ID of the token that has been transferred
      */
     function _afterTokenTransfer(
         address from,
