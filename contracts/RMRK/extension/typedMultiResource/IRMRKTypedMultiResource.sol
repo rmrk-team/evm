cb5a706766072d18da42e818511cc43b45a16087
Jan Turk <jan.turk12@gmail.com>
2022-10-29 23:58:20 +0200

    Fully document typedMultiResource smart contracts with NatSpec
    NatSpec comments have been added to the typedMultiResource smart
    contracts, the existing ones updated and some reclassified.

--- contracts/RMRK/extension/typedMultiResource/IRMRKTypedMultiResource.sol
+++ contracts/RMRK/extension/typedMultiResource/IRMRKTypedMultiResource.sol
@@ -5,9 +5,16 @@ pragma solidity ^0.8.15;
 // import "../../multiresource/IRMRKMultiResource.sol";
 import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 
+/**
+ * @title IRMRKTypedMultiResource
+ * @author RMRK team
+ * @notice Interface smart contract of the RMRK typed multi resource module.
+ */
 interface IRMRKTypedMultiResource is IERC165 {
     /**
-     * @notice Returns type of the resource
+     * @notice Used to get the type of the resource.
+     * @param resourceId ID of the resource to check
+     * @return string The type of the resource
      */
     function getResourceType(uint64 resourceId)
         external
--- contracts/RMRK/extension/typedMultiResource/RMRKTypedMultiResource.sol
+++ contracts/RMRK/extension/typedMultiResource/RMRKTypedMultiResource.sol
@@ -4,9 +4,17 @@ pragma solidity ^0.8.15;
 
 import "./IRMRKTypedMultiResource.sol";
 
+/**
+ * @title RMRKTypedMultiResource
+ * @author RMRK team
+ * @notice Smart contract of the RMRK Typed multi resource module.
+ */
 contract RMRKTypedMultiResource is IRMRKTypedMultiResource {
     mapping(uint64 => string) private _resourceTypes;
 
+    /**
+     * @inheritdoc IERC165
+     */
     function supportsInterface(bytes4 interfaceId)
         public
         view
@@ -16,6 +24,11 @@ contract RMRKTypedMultiResource is IRMRKTypedMultiResource {
         return interfaceId == type(IRMRKTypedMultiResource).interfaceId;
     }
 
+    /**
+     * @notice Used to get the type of the resource.
+     * @param resourceId ID of the resource to check
+     * @return string The type of the resource
+     */
     function getResourceType(uint64 resourceId)
         public
         view
@@ -24,6 +37,11 @@ contract RMRKTypedMultiResource is IRMRKTypedMultiResource {
         return _resourceTypes[resourceId];
     }
 
+    /**
+     * @notice Used to set the type of the resource.
+     * @param resourceId ID of the resource for which the type is being set
+     * @param type_ The type of the resource
+     */
     function _setResourceType(uint64 resourceId, string memory type_) internal {
         _resourceTypes[resourceId] = type_;
     }
