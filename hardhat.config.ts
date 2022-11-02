f05e883664ad8cbf93daf6035e6e6a7bc6bb568c
Jan Turk <jan.turk12@gmail.com>
2022-11-03 00:06:26 +0100

    Add dodoc dependency
    The dodoc dependency has been added to hardhat in order to autogenerate
    the documentation from the NatSpec comments.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,6 +7,7 @@ import '@typechain/hardhat';
 import 'hardhat-gas-reporter';
 import 'solidity-coverage';
 import 'hardhat-contract-sizer';
+import '@primitivefi/hardhat-dodoc';
 
 dotenv.config();
 
