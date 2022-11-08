fc17bb24e99d083e1712082fb2089e647f987aac
steven2308 <steven2308@outlook.com>
2022-11-08 09:21:57 -0500

    Bumps version to 0.14.0.

--- contracts/RMRK/core/RMRKCore.sol
+++ contracts/RMRK/core/RMRKCore.sol
@@ -12,7 +12,7 @@ import "./IRMRKCore.sol";
  */
 contract RMRKCore is IRMRKCore {
     /// @notice Version of the @rmrk-team/evm-contracts package
-    string public constant VERSION = "0.13.1";
+    string public constant VERSION = "0.14.0";
 
     constructor(string memory name_, string memory symbol_) {
         _name = name_;
