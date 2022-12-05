0166df9a70264e0fb0b7a87abcf2129efafe5a85
Jan Turk <jan.turk12@gmail.com>
2022-12-05 22:58:39 +0100

    Update landing README & remove Ropsten
    Landing README has been updated to acknowledge the namechange of the
    legos.
    
    Ropsten network has been removed and the Görli network has replaced the
    references to it.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -35,10 +35,6 @@ const config: HardhatUserConfig = {
     },
   },
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || '',
-      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
     goerli: {
       url: process.env.GOERLI_URL || '',
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
