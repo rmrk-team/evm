8c0a02147f78471449a71e4e0ac5c2d97406879e
steven2308 <steven2308@outlook.com>
2022-12-13 09:19:03 -0500

    Fixes issues and caches strings values for gas savings.

--- contracts/RMRK/extension/tokenProperties/IRMRKTokenProperties.sol
+++ contracts/RMRK/extension/tokenProperties/IRMRKTokenProperties.sol
@@ -10,14 +10,16 @@ import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
  * @notice Interface smart contract of the RMRK token properties extension.
  */
 interface IRMRKTokenProperties is IERC165 {
-
     /**
      * @notice Used to retrieve the string type token properties.
      * @param tokenId The token ID
      * @param key The key of the property
      * @return string The value of the string property
      */
-    function getStringTokenProperty(uint256 tokenId, string memory key) external view returns (string memory);
+    function getStringTokenProperty(
+        uint256 tokenId,
+        string memory key
+    ) external view returns (string memory);
 
     /**
      * @notice Used to retrieve the uint type token properties.
@@ -25,7 +27,10 @@ interface IRMRKTokenProperties is IERC165 {
      * @param key The key of the property
      * @return uint256 The value of the uint property
      */
-    function getUintTokenProperty(uint256 tokenId, string memory key) external view returns (uint256);
+    function getUintTokenProperty(
+        uint256 tokenId,
+        string memory key
+    ) external view returns (uint256);
 
     /**
      * @notice Used to retrieve the bool type token properties.
@@ -33,7 +38,10 @@ interface IRMRKTokenProperties is IERC165 {
      * @param key The key of the property
      * @return bool The value of the bool property
      */
-    function getBoolTokenProperty(uint256 tokenId, string memory key) external view returns (bool);
+    function getBoolTokenProperty(
+        uint256 tokenId,
+        string memory key
+    ) external view returns (bool);
 
     /**
      * @notice Used to retrieve the address type token properties.
@@ -41,7 +49,10 @@ interface IRMRKTokenProperties is IERC165 {
      * @param key The key of the property
      * @return address The value of the address property
      */
-    function getAddressTokenProperty(uint256 tokenId, string memory key) external view returns (address);
+    function getAddressTokenProperty(
+        uint256 tokenId,
+        string memory key
+    ) external view returns (address);
 
     /**
      * @notice Used to retrieve the bytes type token properties.
@@ -49,7 +60,10 @@ interface IRMRKTokenProperties is IERC165 {
      * @param key The key of the property
      * @return bytes The value of the bytes property
      */
-    function getBytesTokenProperty(uint256 tokenId, string memory key) external view returns (bytes memory);
+    function getBytesTokenProperty(
+        uint256 tokenId,
+        string memory key
+    ) external view returns (bytes memory);
 
     /**
      * @notice Used to set a number property.
@@ -57,15 +71,23 @@ interface IRMRKTokenProperties is IERC165 {
      * @param key The property key
      * @param value The property value
      */
-    function setUintProperty(uint256 tokenId, string memory key, uint256 value) external;
+    function setUintProperty(
+        uint256 tokenId,
+        string memory key,
+        uint256 value
+    ) external;
 
     /**
-    * @notice Used to set a string property.
-    * @param tokenId The token ID
-    * @param key The property key
-    * @param value The property value
-    */
-    function setStringProperty(uint256 tokenId, string memory key, string memory value) external;
+     * @notice Used to set a string property.
+     * @param tokenId The token ID
+     * @param key The property key
+     * @param value The property value
+     */
+    function setStringProperty(
+        uint256 tokenId,
+        string memory key,
+        string memory value
+    ) external;
 
     /**
      * @notice Used to set a boolean property.
@@ -73,7 +95,11 @@ interface IRMRKTokenProperties is IERC165 {
      * @param key The property key
      * @param value The property value
      */
-    function setBoolProperty(uint256 tokenId, string memory key, bool value) external;
+    function setBoolProperty(
+        uint256 tokenId,
+        string memory key,
+        bool value
+    ) external;
 
     /**
      * @notice Used to set an bytes property.
@@ -81,14 +107,21 @@ interface IRMRKTokenProperties is IERC165 {
      * @param key The property key
      * @param value The property value
      */
-    function setBytesProperty(uint256 tokenId, string memory key, bytes memory value) external;
+    function setBytesProperty(
+        uint256 tokenId,
+        string memory key,
+        bytes memory value
+    ) external;
 
     /**
-    * @notice Used to set an address property.
+     * @notice Used to set an address property.
      * @param tokenId The token ID
      * @param key The property key
      * @param value The property value
      */
-    function setAddressProperty(uint256 tokenId, string memory key, address value) external;
-
+    function setAddressProperty(
+        uint256 tokenId,
+        string memory key,
+        address value
+    ) external;
 }
--- contracts/mocks/extensions/tokenProperties/RMRKTokenPropertiesMock.sol
+++ contracts/mocks/extensions/tokenProperties/RMRKTokenPropertiesMock.sol
@@ -10,12 +10,16 @@ import "../../../RMRK/extension/tokenProperties/IRMRKTokenProperties.sol";
  * @notice Smart contract of the RMRK Token properties module.
  */
 contract RMRKTokenPropertiesMock is IRMRKTokenProperties {
-
     mapping(string => uint256) private _keysToIds;
-    uint256 private _totalProperties = 0;
+    uint256 private _totalProperties;
 
-    mapping(string => uint256) private _stringValuesToIds;
-    mapping(uint256 => mapping(uint256 => string)) private _stringValues;
+    // For strings, we also use a mapping from strings to Ids, together with a reverse mapping
+    // The purpose is to store unique string values only once since they are more expensive,
+    // and storing only ids.
+    uint256 private _totalStringValues;
+    mapping(string => uint256) private _stringValueToId;
+    mapping(uint256 => string) private _stringIdToValue;
+    mapping(uint256 => mapping(uint256 => uint256)) private _stringValueIds;
 
     mapping(uint256 => mapping(uint256 => address)) private _addressValues;
     mapping(uint256 => mapping(uint256 => bytes)) private _bytesValues;
@@ -23,13 +27,17 @@ contract RMRKTokenPropertiesMock is IRMRKTokenProperties {
     mapping(uint256 => mapping(uint256 => bool)) private _boolValues;
 
     /**
-    * @notice Used to retrieve the string type token properties.
+     * @notice Used to retrieve the string type token properties.
      * @param tokenId The token ID
      * @param key The key of the property
      * @return string The value of the string property
      */
-    function getStringTokenProperty(uint256 tokenId, string memory key) external view returns (string memory) {
-        return _stringValues[tokenId][_keysToIds[key]];
+    function getStringTokenProperty(
+        uint256 tokenId,
+        string memory key
+    ) external view returns (string memory) {
+        uint256 idForValue = _stringValueIds[tokenId][_keysToIds[key]];
+        return _stringIdToValue[idForValue];
     }
 
     /**
@@ -38,7 +46,10 @@ contract RMRKTokenPropertiesMock is IRMRKTokenProperties {
      * @param key The key of the property
      * @return uint256 The value of the uint property
      */
-    function getUintTokenProperty(uint256 tokenId, string memory key) external view returns (uint256) {
+    function getUintTokenProperty(
+        uint256 tokenId,
+        string memory key
+    ) external view returns (uint256) {
         return _uintValues[tokenId][_keysToIds[key]];
     }
 
@@ -48,7 +59,10 @@ contract RMRKTokenPropertiesMock is IRMRKTokenProperties {
      * @param key The key of the property
      * @return bool The value of the bool property
      */
-    function getBoolTokenProperty(uint256 tokenId, string memory key) external view returns (bool) {
+    function getBoolTokenProperty(
+        uint256 tokenId,
+        string memory key
+    ) external view returns (bool) {
         return _boolValues[tokenId][_keysToIds[key]];
     }
 
@@ -58,7 +72,10 @@ contract RMRKTokenPropertiesMock is IRMRKTokenProperties {
      * @param key The key of the property
      * @return address The value of the address property
      */
-    function getAddressTokenProperty(uint256 tokenId, string memory key) external view returns (address) {
+    function getAddressTokenProperty(
+        uint256 tokenId,
+        string memory key
+    ) external view returns (address) {
         return _addressValues[tokenId][_keysToIds[key]];
     }
 
@@ -68,7 +85,10 @@ contract RMRKTokenPropertiesMock is IRMRKTokenProperties {
      * @param key The key of the property
      * @return bytes The value of the bytes property
      */
-    function getBytesTokenProperty(uint256 tokenId, string memory key) external view returns (bytes memory) {
+    function getBytesTokenProperty(
+        uint256 tokenId,
+        string memory key
+    ) external view returns (bytes memory) {
         return _bytesValues[tokenId][_keysToIds[key]];
     }
 
@@ -78,33 +98,28 @@ contract RMRKTokenPropertiesMock is IRMRKTokenProperties {
      * @param key The property key
      * @param value The property value
      */
-    function setUintProperty(uint256 tokenId, string memory key, uint256 value) external {
+    function setUintProperty(
+        uint256 tokenId,
+        string memory key,
+        uint256 value
+    ) external {
         _uintValues[tokenId][_getIdForKey(key)] = value;
     }
 
     /**
-    * @notice Used to set a string property.
-    * @param tokenId The token ID
-    * @param key The property key
-    * @param value The property value
-    */
-    function setStringProperty(uint256 tokenId, string memory key, string memory value) external {
-
-        if (_stringValuesToIds[value] == 0 && _keysToIds[key] == 0) {
-            _keysToIds[key] = _totalProperties;
-            _stringValues[tokenId][_keysToIds[key]] = value;
-            _stringValuesToIds[value] = _totalProperties;
-            _totalProperties++;
-        }
-        else {
-            //prevents storing duplicate string values and keys
-            if (_stringValuesToIds[value] > 0 && _keysToIds[key] == 0) {
-                _keysToIds[key] = _stringValuesToIds[value];
-            }
-            else {
-                _stringValuesToIds[value] = _keysToIds[key];
-            }
-        }
+     * @notice Used to set a string property.
+     * @param tokenId The token ID
+     * @param key The property key
+     * @param value The property value
+     */
+    function setStringProperty(
+        uint256 tokenId,
+        string memory key,
+        string memory value
+    ) external {
+        _stringValueIds[tokenId][_getIdForKey(key)] = _getStringIdForValue(
+            value
+        );
     }
 
     /**
@@ -113,7 +128,11 @@ contract RMRKTokenPropertiesMock is IRMRKTokenProperties {
      * @param key The property key
      * @param value The property value
      */
-    function setBoolProperty(uint256 tokenId, string memory key, bool value) external {
+    function setBoolProperty(
+        uint256 tokenId,
+        string memory key,
+        bool value
+    ) external {
         _boolValues[tokenId][_getIdForKey(key)] = value;
     }
 
@@ -123,38 +142,71 @@ contract RMRKTokenPropertiesMock is IRMRKTokenProperties {
      * @param key The property key
      * @param value The property value
      */
-    function setBytesProperty(uint256 tokenId, string memory key, bytes memory value) external {
+    function setBytesProperty(
+        uint256 tokenId,
+        string memory key,
+        bytes memory value
+    ) external {
         _bytesValues[tokenId][_getIdForKey(key)] = value;
     }
 
     /**
-    * @notice Used to set an address property.
+     * @notice Used to set an address property.
      * @param tokenId The token ID
      * @param key The property key
      * @param value The property value
      */
-    function setAddressProperty(uint256 tokenId, string memory key, address value) external {
+    function setAddressProperty(
+        uint256 tokenId,
+        string memory key,
+        address value
+    ) external {
         _addressValues[tokenId][_getIdForKey(key)] = value;
     }
 
+    /**
+     * @notice Used to get the Id for a key. If the key does not exist, a new Id is created.
+     *  Ids are shared among all tokens and types
+     * @param key The property key
+     * @return uint256 The id for the key
+     */
     function _getIdForKey(string memory key) internal returns (uint256) {
         if (_keysToIds[key] == 0) {
             _totalProperties++;
             _keysToIds[key] = _totalProperties;
             return _totalProperties;
-        }
-        else {
+        } else {
             return _keysToIds[key];
         }
     }
 
     /**
-    * @inheritdoc IERC165
+     * @notice Used to get the Id for a string value. If the value does not exist, a new Id is created.
+     *  Ids are shared among all tokens and used only for strings.
+     * @param value The property value
+     * @return uint256 The id for the value
+     */
+    function _getStringIdForValue(
+        string memory value
+    ) internal returns (uint256) {
+        if (_stringValueToId[value] == 0) {
+            _totalStringValues++;
+            _stringValueToId[value] = _totalStringValues;
+            _stringIdToValue[_totalStringValues] = value;
+            return _totalStringValues;
+        } else {
+            return _stringValueToId[value];
+        }
+    }
+
+    /**
+     * @inheritdoc IERC165
      */
     function supportsInterface(
         bytes4 interfaceId
     ) public view virtual returns (bool) {
-        return interfaceId == type(IRMRKTokenProperties).interfaceId || interfaceId == type(IERC165).interfaceId;
+        return
+            interfaceId == type(IRMRKTokenProperties).interfaceId ||
+            interfaceId == type(IERC165).interfaceId;
     }
-
 }
