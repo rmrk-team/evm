d31630780e78df2f587aee9b36ec909e014a42b8
steven2308 <steven2308@outlook.com>
2022-12-13 08:48:48 -0500

    Adds tests breaking token properties when keys are reused.

--- test/extensions/tokenProperties.ts
+++ test/extensions/tokenProperties.ts
@@ -34,6 +34,7 @@ describe.only("RMRKTokenPropertiesMock", async function() {
   describe("RMRKTokenProperties", async function() {
     let owner: SignerWithAddress;
     const tokenId = 1;
+    const tokenId2 = 2;
 
     beforeEach(async function() {
       ({ tokenProperties } = await loadFixture(tokenPropertiesFixture));
@@ -53,8 +54,6 @@ describe.only("RMRKTokenPropertiesMock", async function() {
       await tokenProperties.setUintProperty(tokenId, "health", bn(80));
       await tokenProperties.setBytesProperty(tokenId, "data", "0x1234");
 
-      console.log(await tokenProperties.getUintTokenProperty(tokenId,"health"));
-
       expect(await tokenProperties.getStringTokenProperty(tokenId, "description")).to.eql("test description");
       expect(await tokenProperties.getStringTokenProperty(tokenId, "description1")).to.eql("test description");
       expect(await tokenProperties.getBoolTokenProperty(tokenId, "rare")).to.eql(true);
@@ -67,6 +66,35 @@ describe.only("RMRKTokenPropertiesMock", async function() {
       expect(await tokenProperties.getStringTokenProperty(tokenId, "description")).to.eql("test description update");
     });
 
+    it("can reuse keys and values are fine", async function() {
+      await tokenProperties.setStringProperty(tokenId, "X", "X1");
+      await tokenProperties.setStringProperty(tokenId2, "X", "X2");
+
+      expect(await tokenProperties.getStringTokenProperty(tokenId, "X")).to.eql("X1");
+      expect(await tokenProperties.getStringTokenProperty(tokenId2, "X")).to.eql("X2");
+    });
+
+    it("can reuse keys among different properties and values are fine", async function() {
+      await tokenProperties.setStringProperty(tokenId, "X", "test description");
+      await tokenProperties.setBoolProperty(tokenId, "X", true);
+      await tokenProperties.setAddressProperty(tokenId, "X", owner.address);
+      await tokenProperties.setUintProperty(tokenId, "X", bn(100));
+      await tokenProperties.setBytesProperty(tokenId, "X", "0x1234");
+
+      expect(await tokenProperties.getStringTokenProperty(tokenId, "X")).to.eql("test description");
+      expect(await tokenProperties.getBoolTokenProperty(tokenId, "X")).to.eql(true);
+      expect(await tokenProperties.getAddressTokenProperty(tokenId, "X")).to.eql(owner.address);
+      expect(await tokenProperties.getUintTokenProperty(tokenId, "X")).to.eql(bn(100));
+      expect(await tokenProperties.getBytesTokenProperty(tokenId, "X")).to.eql("0x1234");
+    });
+
+    it("can reuse string values and values are fine", async function() {
+      await tokenProperties.setStringProperty(tokenId, "X", "common string");
+      await tokenProperties.setStringProperty(tokenId2, "Y", "common string");
+
+      expect(await tokenProperties.getStringTokenProperty(tokenId, "X")).to.eql("common string");
+      expect(await tokenProperties.getStringTokenProperty(tokenId2, "Y")).to.eql("common string");
+    });
   });
 })
 ;
