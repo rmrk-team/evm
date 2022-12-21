98dc13ee152b24085da90c2687ae449e579a1442
Yuri <yuripetusko@gmail.com>
2022-12-21 22:13:52 +0000

    update abi generation script

--- scripts/generate_abis.ts
+++ scripts/generate_abis.ts
@@ -1,34 +1,38 @@
 import fs from 'fs';
 import walkSync from 'walk-sync';
+import rimraf from 'rimraf';
 
 const getTheAbi = () => {
   try {
-    const implementations = walkSync(`${process.cwd()}/artifacts/contracts/implementations`, {
-      directories: false,
-    });
+    rimraf(`${process.cwd()}/artifacts/abis`, () => {
+      const implementations = walkSync(`${process.cwd()}/artifacts/contracts/implementations`, {
+        directories: false,
+      });
 
-    const dirExists = fs.existsSync(`${process.cwd()}/artifacts/abis/implementations`);
-    if (!dirExists) {
-      fs.mkdirSync(`${process.cwd()}/artifacts/abis`);
-      fs.mkdirSync(`${process.cwd()}/artifacts/abis/implementations`);
-      fs.mkdirSync(`${process.cwd()}/artifacts/abis/implementations/abstracts`);
-      fs.mkdirSync(`${process.cwd()}/artifacts/abis/implementations/erc20Pay`);
-    }
-
-    implementations.forEach((implementation) => {
-      const filename = implementation.slice(0, implementation.indexOf('.sol'));
-      const file = fs.readFileSync(
-        `${process.cwd()}/artifacts/contracts/implementations/${implementation}`,
-        'utf8',
-      );
-      const json = JSON.parse(file);
-
-      if (json.abi) {
-        fs.writeFileSync(
-          `${process.cwd()}/artifacts/abis/implementations/${filename}.json`,
-          JSON.stringify(json.abi),
-        );
+      const dirExists = fs.existsSync(`${process.cwd()}/artifacts/abis/implementations`);
+      if (!dirExists) {
+        fs.mkdirSync(`${process.cwd()}/artifacts/abis`);
+        fs.mkdirSync(`${process.cwd()}/artifacts/abis/implementations`);
+        fs.mkdirSync(`${process.cwd()}/artifacts/abis/implementations/abstracts`);
+        fs.mkdirSync(`${process.cwd()}/artifacts/abis/implementations/erc20Pay`);
+        fs.mkdirSync(`${process.cwd()}/artifacts/abis/implementations/premint`);
       }
+
+      implementations.forEach((implementation) => {
+        const filename = implementation.slice(0, implementation.indexOf('.sol'));
+        const file = fs.readFileSync(
+          `${process.cwd()}/artifacts/contracts/implementations/${implementation}`,
+          'utf8',
+        );
+        const json = JSON.parse(file);
+
+        if (json.abi) {
+          fs.writeFileSync(
+            `${process.cwd()}/artifacts/abis/implementations/${filename}.json`,
+            JSON.stringify(json.abi),
+          );
+        }
+      });
     });
   } catch (e) {
     console.log(`e`, e);
