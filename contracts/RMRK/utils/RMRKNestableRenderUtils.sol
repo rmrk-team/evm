b2fc89d296b113553abb504f91fe57daf2ca9bca
Jan Turk <jan.turk12@gmail.com>
2023-05-10 09:54:21 +0200

    Merge NestableUtils and NestableRenderUtils
    As we use RenderUtils for functional utilities as well, the
    NestableUtils were merged into NestableRenderUtils.

--- contracts/RMRK/utils/RMRKNestableRenderUtils.sol
+++ contracts/RMRK/utils/RMRKNestableRenderUtils.sol
@@ -113,4 +113,78 @@ contract RMRKNestableRenderUtils {
         if (parentAddress != expectedParent || expectedParentId != parentId)
             revert RMRKUnexpectedParent();
     }
+
+    /**
+     * @notice Used to validate whether the specified child token is owned by a given parent token.
+     * @param parentAddress Address of the parent token's collection smart contract
+     * @param childAddress Address of the child token's collection smart contract
+     * @param parentId ID of the parent token
+     * @param childId ID of the child token
+     * @return A boolean value indicating whether the child token is owned by the parent token or not
+     */
+    function validateChildOf(
+        address parentAddress,
+        address childAddress,
+        uint256 parentId,
+        uint256 childId
+    ) public view returns (bool) {
+        if (
+            !IERC165(childAddress).supportsInterface(type(IERC6059).interfaceId)
+        ) {
+            return false;
+        }
+
+        (address directOwner, uint256 ownerId, ) = IERC6059(childAddress)
+            .directOwnerOf(childId);
+
+        return (directOwner == parentAddress && ownerId == parentId);
+    }
+
+    /**
+     * @notice Used to validate whether the specified child token is owned by a given parent token.
+     * @param parentAddress Address of the parent token's collection smart contract
+     * @param childAddresses An array of the child token's collection smart contract addresses
+     * @param parentId ID of the parent token
+     * @param childIds An array of child token IDs to verify
+     * @return A boolean value indicating whether all of the child tokens are owned by the parent token or not
+     * @return An array of boolean values indicating whether each of the child tokens are owned by the parent token or
+     *  not
+     */
+    function validateChildrenOf(
+        address parentAddress,
+        address[] memory childAddresses,
+        uint256 parentId,
+        uint256[] memory childIds
+    ) public view returns (bool, bool[] memory) {
+        if (childAddresses.length != childIds.length) {
+            revert RMRKMismachedArrayLength();
+        }
+
+        address directOwner;
+        uint256 ownerId;
+        bool[] memory validityOfChildren = new bool[](childAddresses.length);
+        bool isValid = true;
+
+        for (uint256 i; i < childAddresses.length; ) {
+            validityOfChildren[i] = validateChildOf(
+                parentAddress,
+                childAddresses[i],
+                parentId,
+                childIds[i]
+            );
+
+            if (isValid && !validityOfChildren[i]) {
+                isValid = false;
+            }
+
+            delete directOwner;
+            delete ownerId;
+
+            unchecked {
+                ++i;
+            }
+        }
+
+        return (isValid, validityOfChildren);
+    }
 }
--- docs/RMRK/utils/RMRKEquipRenderUtils.md
+++ docs/RMRK/utils/RMRKEquipRenderUtils.md
@@ -637,6 +637,57 @@ Used to split slot and fixed parts.
 | slotPartIds | uint64[] | An array of IDs of the `Slot` parts included in the `allPartIds` |
 | fixedPartIds | uint64[] | An array of IDs of the `Fixed` parts included in the `allPartIds` |
 
+### validateChildOf
+
+```solidity
+function validateChildOf(address parentAddress, address childAddress, uint256 parentId, uint256 childId) external view returns (bool)
+```
+
+Used to validate whether the specified child token is owned by a given parent token.
+
+
+
+#### Parameters
+
+| Name | Type | Description |
+|---|---|---|
+| parentAddress | address | Address of the parent token&#39;s collection smart contract |
+| childAddress | address | Address of the child token&#39;s collection smart contract |
+| parentId | uint256 | ID of the parent token |
+| childId | uint256 | ID of the child token |
+
+#### Returns
+
+| Name | Type | Description |
+|---|---|---|
+| _0 | bool | A boolean value indicating whether the child token is owned by the parent token or not |
+
+### validateChildrenOf
+
+```solidity
+function validateChildrenOf(address parentAddress, address[] childAddresses, uint256 parentId, uint256[] childIds) external view returns (bool, bool[])
+```
+
+Used to validate whether the specified child token is owned by a given parent token.
+
+
+
+#### Parameters
+
+| Name | Type | Description |
+|---|---|---|
+| parentAddress | address | Address of the parent token&#39;s collection smart contract |
+| childAddresses | address[] | An array of the child token&#39;s collection smart contract addresses |
+| parentId | uint256 | ID of the parent token |
+| childIds | uint256[] | An array of child token IDs to verify |
+
+#### Returns
+
+| Name | Type | Description |
+|---|---|---|
+| _0 | bool | A boolean value indicating whether all of the child tokens are owned by the parent token or not |
+| _1 | bool[] | An array of boolean values indicating whether each of the child tokens are owned by the parent token or  not |
+
 
 
 
@@ -649,6 +700,17 @@ error RMRKChildNotFoundInParent()
 ```
 
 Attempting to find the index of a child token on a parent which does not own it.
+
+
+
+
+### RMRKMismachedArrayLength
+
+```solidity
+error RMRKMismachedArrayLength()
+```
+
+Attempting to pass complementary arrays of different lengths
 
 
 
