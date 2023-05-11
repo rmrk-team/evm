af89530cd03368ecb629b25a99baa121ca855f27
steven2308 <steven2308@outlook.com>
2023-05-11 11:48:38 -0500

    Updates to latest version of EIP6454.

--- contracts/RMRK/extension/soulbound/RMRKSoulbound.sol
+++ contracts/RMRK/extension/soulbound/RMRKSoulbound.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.18;
 
 import "../../core/RMRKCore.sol";
-import "./IERC6454alpha.sol";
+import "./IERC6454beta.sol";
 import "../../library/RMRKErrors.sol";
 
 /**
@@ -11,7 +11,7 @@ import "../../library/RMRKErrors.sol";
  * @author RMRK team
  * @notice Smart contract of the RMRK Soulbound module.
  */
-abstract contract RMRKSoulbound is IERC6454alpha, RMRKCore {
+abstract contract RMRKSoulbound is IERC6454beta, RMRKCore {
     /**
      * @notice Hook that is called before any token transfer. This includes minting and burning.
      * @dev This is a hook ensuring that all transfers of tokens are reverted if the token is soulbound.
@@ -25,28 +25,19 @@ abstract contract RMRKSoulbound is IERC6454alpha, RMRKCore {
         address to,
         uint256 tokenId
     ) internal virtual override {
-        if (
-            from != address(0) && // Exclude minting
-            to != address(0) && // Exclude Burning
-            !isTransferable(tokenId)
-        ) revert RMRKCannotTransferSoulbound();
+        if (!isTransferable(tokenId, from, to))
+            revert RMRKCannotTransferSoulbound();
 
         super._beforeTokenTransfer(from, to, tokenId);
     }
 
-    /**
-     * @inheritdoc IERC6454alpha
-     */
-    function isTransferable(uint256) public view virtual returns (bool) {
-        return false;
-    }
-
     function isTransferable(
-        uint256 tokenId,
-        address,
-        address
+        uint256,
+        address from,
+        address to
     ) public view virtual returns (bool) {
-        return isTransferable(tokenId);
+        return ((from == address(0) || // Exclude minting
+            to == address(0)) && from != to); // Exclude Burning // Besides the obvious transfer to self, if both are address 0 (general transferability check), it returns false
     }
 
     /**
@@ -55,6 +46,6 @@ abstract contract RMRKSoulbound is IERC6454alpha, RMRKCore {
     function supportsInterface(
         bytes4 interfaceId
     ) public view virtual returns (bool) {
-        return interfaceId == type(IERC6454alpha).interfaceId;
+        return interfaceId == type(IERC6454beta).interfaceId;
     }
 }
