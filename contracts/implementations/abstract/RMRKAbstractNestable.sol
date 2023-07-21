bbf0543b28b08d971cd984224f2bdeed305b8525
steven2308 <steven2308@outlook.com>
2023-07-21 11:07:13 -0500

    Adds Nestable only implementations.

---
+++ contracts/implementations/abstract/RMRKAbstractNestable.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: Apache-2.0
+
+pragma solidity ^0.8.18;
+
+// For some reason, adding it to Core produces "Linearization of inheritance graph impossible"
+import "../../RMRK/extension/RMRKRoyalties.sol";
+import "../../RMRK/nestable/RMRKNestable.sol";
+import "../utils/RMRKImplementationBase.sol";
+
+abstract contract RMRKAbstractNestable is
+    RMRKRoyalties,
+    RMRKImplementationBase,
+    RMRKNestable
+{
+    /**
+     * @inheritdoc IERC165
+     */
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view virtual override(IERC165, RMRKNestable) returns (bool) {
+        return
+            super.supportsInterface(interfaceId) ||
+            interfaceId == type(IERC2981).interfaceId ||
+            interfaceId == RMRK_INTERFACE;
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal virtual override {
+        super._beforeTokenTransfer(from, to, tokenId);
+        if (to == address(0)) {
+            unchecked {
+                _totalSupply -= 1;
+            }
+        }
+    }
+
+    /**
+     * @inheritdoc RMRKRoyalties
+     */
+    function updateRoyaltyRecipient(
+        address newRoyaltyRecipient
+    ) public virtual override onlyOwner {
+        _setRoyaltyRecipient(newRoyaltyRecipient);
+    }
+}
---
+++ contracts/implementations/lazyMintErc20/RMRKNestableLazyMintErc20Soulbound.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: Apache-2.0
+
+pragma solidity ^0.8.18;
+
+import "./RMRKNestableLazyMintErc20.sol";
+import "../../RMRK/extension/soulbound/RMRKSoulbound.sol";
+
+contract RMRKNestableLazyMintErc20Soulbound is
+    RMRKSoulbound,
+    RMRKNestableLazyMintErc20
+{
+    constructor(
+        string memory name,
+        string memory symbol,
+        string memory collectionMetadata,
+        string memory baseTokenURI,
+        InitData memory data
+    )
+        RMRKNestableLazyMintErc20(
+            name,
+            symbol,
+            collectionMetadata,
+            baseTokenURI,
+            data
+        )
+    {}
+
+    /**
+     * @inheritdoc IERC165
+     */
+    function supportsInterface(
+        bytes4 interfaceId
+    )
+        public
+        view
+        virtual
+        override(RMRKSoulbound, RMRKAbstractNestable)
+        returns (bool)
+    {
+        return
+            RMRKAbstractNestable.supportsInterface(interfaceId) ||
+            RMRKSoulbound.supportsInterface(interfaceId);
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal virtual override(RMRKSoulbound, RMRKAbstractNestable) {
+        RMRKSoulbound._beforeTokenTransfer(from, to, tokenId);
+        RMRKAbstractNestable._beforeTokenTransfer(from, to, tokenId);
+    }
+}
