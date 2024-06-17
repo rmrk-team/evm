1220e5225b7bfaa227e35a87b654570783b57107
steven2308 <steven2308@outlook.com>
2024-06-17 11:26:57 +0200

    Adds RmrkEquippable version where children are directly accepted.
    This saves about 40% of gas since accept operation is not needed,
    and pending children array is not used.
    
    It also saves 4.5% of contract size.

---
+++ contracts/mocks/RMRKMinifiedEquippableChildAutoAcceptMock.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: Apache-2.0
+
+pragma solidity ^0.8.21;
+
+import {
+    RMRKMinifiedEquippableChildAutoAccept
+} from "../RMRK/equippable/RMRKMinifiedEquippableChildAutoAccept.sol";
+
+//Minimal public implementation of RMRKEquippable for testing.
+contract RMRKMinifiedEquippableChildAutoAcceptMock is
+    RMRKMinifiedEquippableChildAutoAccept
+{
+    function safeMint(address to, uint256 tokenId) public {
+        _safeMint(to, tokenId, "");
+    }
+
+    function safeMint(address to, uint256 tokenId, bytes memory _data) public {
+        _safeMint(to, tokenId, _data);
+    }
+
+    function mint(address to, uint256 tokenId) external {
+        _safeMint(to, tokenId, "");
+    }
+
+    function nestMint(
+        address to,
+        uint256 tokenId,
+        uint256 destinationId
+    ) external {
+        _nestMint(to, tokenId, destinationId, "");
+    }
+
+    function addAssetToToken(
+        uint256 tokenId,
+        uint64 assetId,
+        uint64 replacesAssetWithId
+    ) external {
+        _addAssetToToken(tokenId, assetId, replacesAssetWithId);
+    }
+
+    function addEquippableAssetEntry(
+        uint64 id,
+        uint64 equippableGroupId,
+        address catalogAddress,
+        string memory metadataURI,
+        uint64[] calldata partIds
+    ) external {
+        _addAssetEntry(
+            id,
+            equippableGroupId,
+            catalogAddress,
+            metadataURI,
+            partIds
+        );
+    }
+
+    function setValidParentForEquippableGroup(
+        uint64 equippableGroupId,
+        address parentAddress,
+        uint64 partId
+    ) external {
+        _setValidParentForEquippableGroup(
+            equippableGroupId,
+            parentAddress,
+            partId
+        );
+    }
+}
